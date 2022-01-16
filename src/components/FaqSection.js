8519d89a2cb13ebd1ce7d05a830d98aba0a93a74
Stopalov12 <stopalov111@gmail.com>
2022-01-16 12:44:42 +0200

    4. FAQ Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+            dolorem?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+            dolorem?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+            dolorem?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer</h4>
+        <div className="answer">
+          <p>Lorem, ipsum dolor.</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+            dolorem?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,13 +2,16 @@ import React from 'react';
 //Page Components
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
 
 const AboutUs = () => {
-  return(<>
-    <AboutSection />;
-    <ServicesSection />;
-  </>)
-  
+  return (
+    <>
+      <AboutSection />;
+      <ServicesSection />;
+      <FaqSection />
+    </>
+  );
 };
 
 export default AboutUs;
