e67135dff7d08f296f2326f436fc7697e860196b
Stopalov12 <stopalov111@gmail.com>
2022-01-16 12:24:34 +0200

    2. About Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
+import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <div className="App">
- <h1>Film</h1>
-      
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutSection.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import home1 from '../img/home1.png';
+const AboutSection = () => {
+  return (
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>We work to make</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </div>
+          <div className="hide">
+            <h2>true.</h2>
+          </div>
+        </div>
+        <p>
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills.
+        </p>
+        <button>Contact us</button>
+      </div>
+      <img src={home1} alt="guy with a camera" />
+    </div>
+  );
+};
+
+export default AboutSection;
